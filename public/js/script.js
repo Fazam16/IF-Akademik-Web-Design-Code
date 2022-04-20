82afd64aca5feff0c335df2f07b645a8b15c2fb4
Fazam16 <fadilzahran0123@gmail.com>
2022-04-20 16:15:39 +0700

    Finalisasi

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,10 @@ const drop = document.querySelector('#drop')
 kategori.addEventListener('click', function() {
     drop.classList.toggle('hidden');
 });
+
+const kategori1 = document.querySelector('#kategori1');
+const drop1 = document.querySelector('#drop1')
+
+kategori1.addEventListener('click', function() {
+    drop1.classList.toggle('hidden');
+});
