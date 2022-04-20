592ab0a4359187d359cc476cadce5e5806b670da
Fazam16 <fadilzahran0123@gmail.com>
2022-04-20 15:46:59 +0700

    Menambahkan drop down kategori

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,10 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+const kategori = document.querySelector('#kategori');
+const drop = document.querySelector('#drop')
+
+kategori.addEventListener('click', function() {
+    drop.classList.toggle('hidden');
+});
